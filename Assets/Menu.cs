5a008c83d197f1ee380bfcd2d44310ef44cdea7b
Nero-X <SashaJCV@yandex.ru>
2019-02-28 23:45:25 +0300

    //

---
+++ Assets/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button exit; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exit.onClick.AddListener(Close);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Close()
+    {
+        Application.Quit();
+    }
+}
