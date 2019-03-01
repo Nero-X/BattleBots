bb15b61f329be3aca2b8f9026092d1110778eda1
Nero-X <SashaJCV@yandex.ru>
2019-03-01 22:22:34 +0300

    Added 2-nd scene

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public Button exit; 
+    public Button exit;
+    public Button next;
+    public Scene setup;
 
     // Start is called before the first frame update
     void Start()
     {
         exit.onClick.AddListener(Close);
+        next.onClick.AddListener(Next);
     }
 
     // Update is called once per frame
@@ -22,5 +24,10 @@ public class Menu : MonoBehaviour
     void Close()
     {
         Application.Quit();
+    }
+
+    void Next()
+    {
+        SceneManager.LoadScene("Setup", LoadSceneMode.Single);
     }
 }
